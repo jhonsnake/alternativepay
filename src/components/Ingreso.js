ea6f9370fbdf6ef130efd8b4e4f76a8d4d376a2b
jhonsnake <jhonprada@gmail.com>
2020-01-10 21:43:52 -0500

    Formulario 1

--- src/components/Ingreso.js
+++ src/components/Ingreso.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+
+function Error(){
+    return(<div className="error">
+        <p className="content__extract animated bounceInUp content--alert">¡Ha habido un error!, recuerda llenar todos los campos, Los datos ingresados no existen... Si estás en un entorno de pruebas intenta con la referencia:123, documento: 123123123</p>
+    </div>)
+}
 
 function Ingreso() {
+    const [error, setError] = useState(false)
+
+    const referencia =123;
+    const documento = 123123123;
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(referenciaInput.current.value != referencia || docInput.current.value != documento){
+            console.log("error")
+            setError(true)
+        }
+    }
+
+    const validateError = ()=>{
+        if(error){
+            return <Error/>
+        }
+
+        return null;
+    }
+
+    const referenciaInput = useRef();
+    const docInput = useRef();
+
+    
     return (
         <div className="ingreso">
             <p className="content__extract">Comienze el proceso de pago ingresando la siguiente información</p>
+            <form className="formulario">
+                <input className="formulario__input" type="text" placeholder="Comercio" value="Comercio de prueba" disabled  />
+                <input className="formulario__input" type="number"  placeholder="Referencia" ref={referenciaInput} />
+                <input className="formulario__input" type="text" placeholder="Documento" ref={docInput} />
+                <button onClick={handleSubmit} className="button button--primary button--center">Ingresar</button>
+            </form>
+            
+            {validateError()}
 
-            </div>
+            
+
+        </div>
     )
 }
 
